8c046958f89c75772adda80f89803cc1e4362cca
elsuizo <mnoblia@disroot.org>
2020-11-10 19:36:47 -0300

    minimal window running and event handling

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,37 @@
+extern crate sfml;
+
+use sfml::graphics::{CircleShape, Color, Font, RectangleShape, RenderTarget,
+                     RenderWindow, Shape,Text, Transformable, Vertex, VertexArray, PrimitiveType};
+
+use sfml::system::{Clock, Time, Vector2f, Vector2u, Vector3f};
+use sfml::window::{VideoMode, ContextSettings, Event, Key, Style};
+
+const WINDOW_WIDTH:  f32 = 500.0;
+const WINDOW_HEIGHT: f32 = 500.0;
+
 fn main() {
-    println!("Hello, world!");
+    let mut window = RenderWindow::new(
+        (WINDOW_WIDTH as u32, WINDOW_HEIGHT as u32),
+        "Double pendulum simulation",
+        Style::CLOSE,
+        &Default::default(),
+    );
+    window.set_vertical_sync_enabled(true);
+    let background_color = Color::BLACK;
+    let mut is_running = true;
+    loop {
+        while let Some(event) = window.poll_event() {
+            match event {
+                Event::Closed | Event::KeyPressed {code: Key::Escape, ..} => return,
+                Event::KeyPressed {code: Key::Space, ..} if !is_running => {
+                    is_running = true;
+                    //clock.restart();
+                    println!("space!!!");
+                }
+                _ => {}
+            }
+        }
+        window.clear(background_color);
+        window.display();
+    }
 }
