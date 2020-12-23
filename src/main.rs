d45c9d9538b9e7e55e85bb26fcc1a8faa797d567
elsuizo <mnoblia@disroot.org>
2020-12-23 12:12:47 -0300

    grid creation done!!!

--- src/main.rs
+++ src/main.rs
@@ -1,29 +1,96 @@
 // TODO(elsuizo:2020-12-20): cosas que faltan:
-// - [  ] Hacer una funcion para graficar la grilla
+// - [X] Hacer una funcion para graficar la grilla
 // - [  ] cuando pasamos con el mouse por una cell tenemos que poder elegirla
 // - [  ] hacer una gui chica para elegir los colores
-use sfml::graphics::{CircleShape, Color, Font, RectangleShape, RenderTarget, Rect,
+use sfml::graphics::{Color, RectangleShape, RenderTarget, Rect,
                      RenderWindow, Shape,Text, Transformable, Vertex, VertexArray, PrimitiveType};
 
 use sfml::system::{Clock, Time, Vector2f, Vector2u, Vector3f, Vector2};
 use sfml::window::{mouse, VideoMode, ContextSettings, Event, Key, Style};
 
+use rand::{Rng};
+
 const WINDOW_WIDTH:  f32 = 800.0;
 const WINDOW_HEIGHT: f32 = 600.0;
 
-const DRAW_GRID_WH: u8 = 16;
-const DRAW_CELL_WH: u8 = 26;
+#[derive(Debug)]
+struct Cell<'a> {
+    shape: RectangleShape<'a>,
+    position: Vector2f,
+    color: Color,
+    active: bool,
+    // id: usize
+}
 
-const DRAW_AREA_TOPLEFT: (u16, u16) = (200, 100);
+// NOTE(elsuizo:2020-12-22): voy a probar teniendo un solo shape para todos los
+// shapes ya que
+impl<'a> Cell<'a> {
+    const WH: u8 = 26;
+
+    fn new(position: Vector2f, color: Color) -> Self {
+        let mut shape = RectangleShape::default();
+        shape.set_position(position);
+        shape.set_outline_thickness(3.0);
+        shape.set_fill_color(color);
+        shape.set_outline_color(Color::BLACK);
+        shape.set_size((Self::WH as f32, Self::WH as f32));
+
+        Self{shape, color, position, active: true}
+    }
+
+}
+
+#[derive(Debug)]
+struct Grid<'a> {
+    cells: Vec<Cell<'a>>,
+}
+
+impl<'a> Grid<'a> {
+    const WH: u8 = 16;
+    const TOPLEFT: (u16, u16) = (200, 100);
+    fn new(cells: Vec<Cell<'a>>) -> Self {
+        Self{cells}
+    }
+
+    fn create_grid() -> Self {
+        let mut cells = Vec::new();
+        for y in 0..Self::WH {
+            for x in 0..Self::WH {
+                let cell_position = Vector2f::new(Self::TOPLEFT.0 as f32 + (x as f32 * Cell::WH as f32),
+                                                  Self::TOPLEFT.1 as f32 + (y as f32 * Cell::WH as f32),);
+                let color = pick_random_color();
+                let mut cell = Cell::new(cell_position, color);
+                cells.push(cell);
+            }
+        }
+        Self::new(cells)
+    }
+}
 
 fn mouse_over(rect: &Rect<i32>, mouse_x: i32, mouse_y: i32) -> bool {
     rect.contains(Vector2::new(mouse_x, mouse_y))
 }
 
-fn gridindex(grid: &mut [bool; DRAW_GRID_WH as usize * DRAW_GRID_WH as usize],
-             x: usize, y: usize) -> Option<&mut bool>
-{
-    grid.get_mut(y * DRAW_GRID_WH as usize + x)
+// fn gridindex(grid: &mut [bool; DRAW_GRID_WH as usize * DRAW_GRID_WH as usize],
+//              x: usize, y: usize) -> Option<&mut bool>
+// {
+//     grid.get_mut(y * DRAW_GRID_WH as usize + x)
+// }
+
+fn gen_random_color() -> Color {
+    let mut rng = rand::thread_rng();
+    Color::rgb(rng.gen_range(0..255), rng.gen_range(0..255), rng.gen_range(0..255))
+}
+
+fn pick_random_color() -> Color {
+    let mut rng = rand::thread_rng();
+    match rng.gen_range(0..5) {
+        0 => Color::RED,
+        1 => Color::GREEN,
+        2 => Color::WHITE,
+        3 => Color::BLUE,
+        _ => gen_random_color()
+    }
 }
 
 fn main() {
@@ -36,14 +103,9 @@ fn main() {
     window.set_vertical_sync_enabled(true);
     let background_color = Color::BLACK;
     let mut is_running = true;
-    // con una shape podemos reutilizarla para cada cell
-    let mut shape = RectangleShape::default();
-    shape.set_outline_thickness(-1.0);
-    shape.set_outline_color(Color::RED);
 
-    let set_button = Rect::new(348, 500, 100, 32);
-    // esta es la grilla que usa de para saber si esta en la cell creo
-    let mut pixel_grid = [false; DRAW_GRID_WH as usize * DRAW_GRID_WH as usize];
+    let grid = Grid::create_grid();
+
     loop {
         while let Some(event) = window.poll_event() {
             match event {
@@ -57,26 +119,9 @@ fn main() {
             }
         }
 
-        let mouse_position = window.mouse_position();
-        // TODO(elsuizo:2020-12-20): esto tiene que ir a una funcion
-        shape.set_fill_color(Color::TRANSPARENT);
-        for y in 0..DRAW_GRID_WH {
-            for x in 0..DRAW_GRID_WH {
-                if let Some(cell) = gridindex(&mut pixel_grid, x as usize, y as usize) {
-                    shape.set_fill_color(Color::RED);
-                    if mouse::Button::Left.is_pressed() {
-                        *cell = false;
-                        shape.set_fill_color(Color::BLACK);
-                    }
-                }
-                shape.set_outline_color(Color::rgb(180, 180, 180));
-                shape.set_size((DRAW_CELL_WH as f32, DRAW_CELL_WH as f32));
-                shape.set_position((
-                    DRAW_AREA_TOPLEFT.0 as f32 + (x as f32 * DRAW_CELL_WH as f32),
-                    DRAW_AREA_TOPLEFT.1 as f32 + (y as f32 * DRAW_CELL_WH as f32),
-                ));
-                window.draw(&shape);
-            }
+        // let mouse_position = window.mouse_position();
+        for cell in &grid.cells {
+            window.draw(&cell.shape);
         }
         window.display();
         window.clear(background_color);
